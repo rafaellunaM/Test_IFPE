67c05bff4efdbcbfea8e7dbaab795b52871537d7
Cesar Rodrigues <134346335+CesinhagGit@users.noreply.github.com>
2024-11-14 09:47:07 -0300

    Update componente2.py

--- Teste_componente/componente2.py
+++ Teste_componente/componente2.py
@@ -1,7 +1,22 @@
+import unittest
 
 def mult(a, b):
     return a * b
 
 def div(a, b):
-    return a / b
+    return a / b 
 
+class Test(unittest.TestCase):
+    
+    def test_multiplica(self):
+        self.assertEqual(mult(2, 3), 6)
+        self.assertEqual(mult(-1, 1), -1)
+        self.assertEqual(mult(0, 0), 0)
+        
+    def test_divide(self):
+        self.assertEqual(div(6, 3), 2)
+        self.assertEqual(div(10, 2), 5)
+        self.assertEqual(div(2, 1), 2)
+
+if __name__ == '__main__':
+    unittest.main()
